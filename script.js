8b513499ef0e007126355cf9f7bcae42b40e39ac
Ligia Monteiro <ligiatmonteiro@gmail.com>
2022-10-16 13:53:52 +0100

    changes to the navbar

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let menu = document.getElementById("menu-bar");
 let item = document.getElementById("item");
 
-item.style.right = '-300px';
+item.style.right = '-800px';
 menu.onclick = function() {
 
-        if (item.style.right == '-300px') {
+        if (item.style.right == '-800px') {
             item.style.right = '0';
         } else {
-            item.style.right = '-300px';
+            item.style.right = '-800px';
         }
     }
+
+item.onclick = function() {
+
+    if (item.style.right == '-800px') {
+        item.style.right = '0';
+    } else {
+        item.style.right = '-800px';
+    }
+}
